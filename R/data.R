7994eb6fa2ae0bdb3bedcdb0dfde8d71f7147656
ondrej-klima <ondra.klima@seznam.cz>
2021-12-21 01:30:27 +0100

    Added documentation for pretrained FPCA models model.bgs.m and model.bgs.f.

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' FPCA model for boys
+#' 
+#' Model trained using 167 male individuals from Brno Growth Study (BGS).
+#' 
+"model.bgs.m"
+
+#' FPCA model for girls
+#' 
+#' Model trained using 167 female individuals from Brno Growth Study (BGS).
+#' 
+"model.bgs.f"
